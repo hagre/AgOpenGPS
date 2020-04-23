2d684bca042f234a62eb8546abfe17950cea2a3d
hagre <hagre@localhost.com>
2020-04-23 09:28:24 +0200

    Insert old folder witch diskspreader from dev repository

---
+++ AgOpenGPS_Dev/SourceCode/GPS/Forms/FormSim.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace AgOpenGPS
+{
+    public partial class FormSim : Form
+    {
+        private readonly FormGPS mf = null;
+
+        //Form stuff
+        public FormSim(Form callingForm)
+        {
+            mf = callingForm as FormGPS;
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# AgOpenGPS_Dev
+
+Nightly test builds of AgOpenGPS. Might work, might not..... 
+
+Use AgOpenGPS for stable release.
